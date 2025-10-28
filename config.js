aea0d9a7eb3c4db52cef900304d91155437c36a6
sandesissu7-collab <sandesissu7@gmail.com>
2025-10-28 16:43:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "gZw1BRBQ#Q9P8BCO6UNXuWvMIfN892P3EZAJgQOc2JrAGrvdcC58",
+SESSION_ID: process.env.SESSION_ID || "6274c8c57fec3c3e",
 ALIVE_IMG : process.env.ALIVE_IMG || "https://files.catbox.moe/4bc81k.png",
 ALIVE_MSG : process.env.ALIVE_MSG || "*👋 ℍ𝔼𝕃𝕃𝕆 𝕋ℍ𝔼ℝ𝔼 𝕀'𝕄 𝔸𝕃𝕀𝕍𝔼 ℕ𝕆𝕎 ,👑 ℚ𝕌𝔼𝔼ℕ 𝕄𝔸𝕐𝔸 𝕄𝔻 𝕎𝕙𝕒𝕥𝕤𝔸𝕡𝕡 𝕓𝕠𝕥 *\n\n*𝕋𝕐ℙ𝔼.𝕄𝔼ℕ𝕌 𝕋𝕆 𝔹𝔼𝔾𝕀ℕ𝔾 *\n\n*⚖️𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 - : ꜱᴀɴᴅᴇꜱ ɪꜱᴜʀᴀɴᴅᴀ ツ",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
