8bb369ed91bfb56c1570c9bdc793bf310f5550da
sandesissu7-collab <sandesissu7@gmail.com>
2025-11-14 12:05:02 +0530

    Update download.js

--- plugins/download.js
+++ plugins/download.js
@@ -1,5 +1,6 @@
-const { cmd } = require('../command');
-const axios = require('axios');
+const { fetchJson } = require('../lib/functions')
+const config = require('../config')
+const { cmd, commands } = require('../command')
 
 cmd({
   pattern: "ig2",
