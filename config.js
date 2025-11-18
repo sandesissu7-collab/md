938c966d1d24a9d846d805a4aee3d0a7bc07a861
sandesissu7-collab <sandesissu7@gmail.com>
2025-11-18 08:41:16 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "OppmAb6K#ICrcEbjOFbYcRs33xu3Qy9sCRBgo9W-bE9xCKJoaNUw",
+ALIVE_IMG : process.env.ALIVE_IMG || "https://files.catbox.moe/4bc81k.png",
+ALIVE_MSG : process.env.ALIVE_MSG || "*👋 ℍ𝔼𝕃𝕃𝕆 𝕋ℍ𝔼ℝ𝔼 𝕀'𝕄 𝔸𝕃𝕀𝕍𝔼 ℕ𝕆𝕎 ,👑 ℚ𝕌𝔼𝔼ℕ 𝕄𝔸𝕐𝔸 𝕄𝔻 𝕎𝕙𝕒𝕥𝕤𝔸𝕡𝕡 𝕓𝕠𝕥 *\n*𝕋𝕐ℙ𝔼.𝕄𝔼ℕ𝕌 𝕋𝕆 𝔹𝔼𝔾𝕀ℕ𝔾 *\n*⚖️𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 - : ꜱᴀɴᴅᴇꜱ ɪꜱᴜʀᴀɴᴅᴀ ツ",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+};
